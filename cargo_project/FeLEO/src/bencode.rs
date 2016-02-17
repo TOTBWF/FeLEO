9f2e3a285b6c2f80737ba14834b8241b54567bf6
JMMyles <JosephMMyles@gmail.com>
2016-02-17 13:01:48 -0600

    Not compiling due to several errors, mostly due to indexing syntax.

--- cargo_project/FeLEO/src/bencode.rs
+++ cargo_project/FeLEO/src/bencode.rs
@@ -40,26 +40,46 @@ pub fn be_bencode(data: Vec<char>, data_len: i64) -> Vec<char> {
         '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' => { // TODO
             let mut i = 0;
             let mut string_len = 0;
-            while data[i] == '0' | data[i] == '1' | data[i] == '2' | data[i] == '3' | data[i] == '4'
-                | data[i] == '5' | data[i] == '6' | data[i] == '7' | data[i] == '8' | data[i] == '9'
+            while data[i] == '0' || data[i] == '1' || data[i] ==  '2' || data[i] == '3' || 
+                  data[i] == '4' || data[i] == '5' || data[i] ==  '6' || data[i] == '7' || 
+                  data[i] == '8' || data[i] == '9'
                 {
                     // cast data[i] as int and add it (w/offset) to string_len
                     i += 1;
             }
             let mut j = 0;
+            i += 1;
             while j < string_len {
                 ret[j] = data[i];
                 j += 1;
                 i += 1;
             }
-            ret
+            return ret;
         }
 
         // lists
-        'l' => { ; } // TODO
+        'l' => { 
+            if data[data_len-1] == 'e' { 
+                // recurse
+                be_bencode(data[1..data_len-2], data_len-2)
+            } else {
+                // error
+                println!("List should end in e");
+                []
+            }
+        } // TODO
 
         // dictionaries
-        'd' => { ; } // TODO
+        'd' => { 
+            if data[data_len-1] == 'e' {
+                // same as 'l'
+                be_bencode(data[1..data_len-2], data_len-2)
+            } else {
+                // error
+                println!("Dictionary should end in e");
+                []
+            }
+        } // TODO
 
         // else: error
         _ => { println!("error: bencoded string not recognized"); }
